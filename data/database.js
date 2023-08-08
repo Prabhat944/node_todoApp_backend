29eab1b30be4fac41a2309787a9d97644d88ac53
Prabhat kumar <prabhat.kumar@dresma.com>
2023-08-08 08:22:37 +0530

    fixed engines for node

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ export const mongoConnect = () => {
     .connect(process.env.MONGO_URI, {
       dbName: "todo",
     })
-    .then(() => {console.log("mongoDb connected successfully")})
+    .then((res) => {console.log(`mongoDb connected successfully with ${res.connection.host}`)})
     .catch((error) => {
       console.log("Something went wrong in mongoDb connection", error);
     });
